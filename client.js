ba72a6c354474fa6900858bad33039c7d3a7e499
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-27 13:50:12 +0000

    update

--- client.js
+++ client.js
@@ -1,23 +1,43 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import { ReactInstance, Surface, Module } from 'react-360-web';
 
-function init(bundle, parent, options = {}) {
-  const r360 = new ReactInstance(bundle, parent, {
-    // Add custom options here
-    fullScreen: true,
-    ...options,
-  });
+class TitleChanger extends Module {
+    constructor() {
+        super('TitleChanger');
+    }
 
-  // Render your app content to the default cylinder surface
-  r360.renderToSurface(
-    r360.createRoot('TravelVR', { /* initial props */ }),
-    r360.getDefaultSurface()
-  );
-
-  // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+    changeTitle(title) {
+        document.title = 'Welcome to ' + title;
+    }
 }
 
-window.React360 = {init};
+function init(bundle, parent, options = {}) {
+    const r360 = new ReactInstance(bundle, parent, {
+        // Add custom options here
+        fullScreen: true,
+        nativeModules: [new TitleChanger()],
+        ...options
+    });
+
+    const myCylinderSurface = new Surface(
+        4680,
+        600,
+        Surface.SurfaceShape.Cylinder
+    );
+
+    // Render your app content to the default cylinder surface
+    r360.renderToSurface(
+        r360.createRoot('TravelVR', {
+            /* initial props */
+        }),
+        myCylinderSurface
+    );
+
+    
+    // Load the initial environment
+    r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+}
+
+window.React360 = { init };
--- index.js
+++ index.js
@@ -1,43 +1,86 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+    AppRegistry,
+    asset,
+    StyleSheet,
+    Environment,
+    Prefetch,
+    View,
+    Image,
+    NativeModules,
+    VrButton
 } from 'react-360';
+import Flag from './components/Flag';
+
+const { TitleChanger } = NativeModules;
+
+const PLACES = [
+    {
+        name: 'Space',
+        flag: 'flag_nasa.png',
+        panorama: 'stars.png'
+    },
+    {
+        name: 'Spain',
+        flag: 'flag_spain.png',
+        panorama: 'spain.jpg'
+    },
+    {
+        name: 'Italy',
+        flag: 'flag_italy.png',
+        panorama: 'italy.jpg'
+    },
+    {
+        name: 'Ukraine',
+        flag: 'flag_ukraine.jpg',
+        panorama: 'ukraine.jpg'
+    }
+];
 
 export default class TravelVR extends React.Component {
-  render() {
-    return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
-        </View>
-      </View>
-    );
-  }
-};
+    state = {
+        activeFlag: ''
+    };
+
+    changeBackground(panorama, name) {
+        Environment.setBackgroundImage(asset(panorama));
+        TitleChanger.changeTitle(name);
+    }
+
+    renderFlags() {
+        return PLACES.map(({ flag, panorama, name }) => {
+            return (
+                <Fragment key={flag}>
+                    <Prefetch source={asset(panorama)} />
+                    <VrButton
+                        onEnter={() => this.setState({ activeFlag: flag })}
+                        onExit={() => this.setState({ activeFlag: '' })}
+                        onClick={() => this.changeBackground(panorama, name)}
+                    >
+                        <Flag image={flag} activeFlag={this.state.activeFlag} />
+                    </VrButton>
+                </Fragment>
+            );
+        });
+    }
+
+    render() {
+        const { flagContainer } = styles;
+
+        return <View style={flagContainer}>{this.renderFlags()}</View>;
+    }
+}
 
 const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
+    flagContainer: {
+        height: 600,
+        width: 4680,
+        backgroundColor: 'rgba(255, 255, 255, 0.3)',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
 
 AppRegistry.registerComponent('TravelVR', () => TravelVR);
+AppRegistry.registerComponent('Flag', () => Flag);
